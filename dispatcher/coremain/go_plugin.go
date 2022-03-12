yyysuo/mosdns

commit acf6de89d14dcc4919ee09ec112fcb734c1c491f

core: enable go plugin by build tag rather than by default

dispatcher/coremain/go_plugin.go
@@ -1,0 +1,28 @@
+//go:build go_plugin
+
+//     Copyright (C) 2020-2021, IrineSistiana
+//
+//     This file is part of mosdns.
+//
+//     mosdns is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     mosdns is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package coremain
+
+var openGoPlugin = func(path string) error {
+	mlog.L().Info("loading library", zap.String("library", lib))
+	_, err := plugin.Open(lib)
+	if err != nil {
+		return fmt.Errorf("failed to open library: %w", err)
+	}
+}

dispatcher/coremain/go_plugin_others.go
@@ -1,0 +1,24 @@
+//go:build !go_plugin
+
+//     Copyright (C) 2020-2021, IrineSistiana
+//
+//     This file is part of mosdns.
+//
+//     mosdns is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     mosdns is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package coremain
+
+var openGoPlugin = func(path string) error {
+	return nil
+}

dispatcher/coremain/run.go
@@ -28,7 +28,6 @@ import (
 	"go.uber.org/zap/zapcore"
 	"os"
 	"os/signal"
-	"plugin"
 	"runtime/debug"
 	"syscall"
 )
@@ -117,10 +116,8 @@ func loadConfig(f string, depth int) error {
 		}
 
 		for _, lib := range c.Library {
-			mlog.L().Info("loading library", zap.String("library", lib))
-			_, err := plugin.Open(lib)
-			if err != nil {
-				return fmt.Errorf("failed to open library: %w", err)
+			if err := openGoPlugin(lib); err != nil {
+				return err
 			}
 		}
 	}

commit 3ff44c3a82e02ea5dd1e88f32ff6591ed11a3998

core: fixed broken code in acf6de89d14dcc4919ee09ec112fcb734c1c491f

dispatcher/coremain/go_plugin.go
@@ -19,10 +19,14 @@
 
 package coremain
 
+import (
+	"github.com/IrineSistiana/mosdns/v3/dispatcher/mlog"
+	"go.uber.org/zap"
+	"plugin"
+)
+
 var openGoPlugin = func(path string) error {
-	mlog.L().Info("loading library", zap.String("library", lib))
-	_, err := plugin.Open(lib)
-	if err != nil {
-		return fmt.Errorf("failed to open library: %w", err)
-	}
+	mlog.L().Info("loading go plugin library", zap.String("library", path))
+	_, err := plugin.Open(path)
+	return err
 }