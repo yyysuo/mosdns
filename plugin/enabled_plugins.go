5b4f59f12b5164de7e93814d83a60f8758cea749
yyysuo <philhorse.cn@gmail.com>
2025-11-28 16:26:17 +0800

    Add switcher10 plugin to enabled plugins list

--- plugin/enabled_plugins.go
+++ plugin/enabled_plugins.go
@@ -70,6 +70,7 @@ import (
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/switcher7"
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/switcher8"
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/switcher9"
+	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/switcher10"
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/aliapi"
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/cname_remover"
 	_ "github.com/IrineSistiana/mosdns/v5/plugin/executable/adguard"
