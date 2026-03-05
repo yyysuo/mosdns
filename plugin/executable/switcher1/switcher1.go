e58d17abb247fe4d0f0ce15be2870ce4f8ba6758
yyysuo <philhorse.cn@gmail.com>
2026-03-05 16:45:49 +0800

    极限加速

--- plugin/executable/switcher1/switcher1.go
+++ plugin/executable/switcher1/switcher1.go
@@ -133,3 +133,9 @@ func (m *switchMatcher1) GetFastCheck() func(qCtx *query_context.Context) bool {
 		return v == exp
 	}
 }
+func (s *Switcher1) GetValue() string {
+	if val, ok := s.value.Load().(string); ok {
+		return val
+	}
+	return ""
+}
