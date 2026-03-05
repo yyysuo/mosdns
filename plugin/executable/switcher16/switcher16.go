090e92332deea82c47328ca196c44aac8746213f
yyysuo <philhorse.cn@gmail.com>
2026-03-05 16:46:02 +0800

    极限加速

--- plugin/executable/switcher16/switcher16.go
+++ plugin/executable/switcher16/switcher16.go
@@ -133,3 +133,9 @@ func (m *switchMatcher16) GetFastCheck() func(qCtx *query_context.Context) bool 
 		return v == exp
 	}
 }
+func (s *Switcher16) GetValue() string {
+	if val, ok := s.value.Load().(string); ok {
+		return val
+	}
+	return ""
+}
