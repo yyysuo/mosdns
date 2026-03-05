f867c122eee4bd3b0fb6a2ba28fabaab1277c6a5
yyysuo <philhorse.cn@gmail.com>
2026-03-05 16:45:33 +0800

    极限加速

--- pkg/server/iface.go
+++ pkg/server/iface.go
@@ -7,14 +7,6 @@ import (
 	"github.com/miekg/dns"
 )
 
-// Handler handles incoming request q and MUST ALWAYS return a response.
-// Handler MUST handle dns errors by itself and return a proper error responses.
-// e.g. Return a SERVFAIL if something goes wrong.
-// If Handle() returns a nil resp, caller will
-// udp: do nothing.
-// tcp/dot: close the connection immediately.
-// doh: send a 500 response.
-// doq: close the stream immediately.
 type Handler interface {
 	Handle(ctx context.Context, q *dns.Msg, meta QueryMeta, packMsgPayload func(m *dns.Msg) (*[]byte, error)) (respPayload *[]byte)
 }
@@ -22,8 +14,9 @@ type Handler interface {
 type QueryMeta struct {
 	FromUDP bool
 
-	// Optional
-	ClientAddr netip.Addr
-	ServerName string
-	UrlPath    string
+	ClientAddr       netip.Addr
+	ServerName       string
+	UrlPath          string
+	PreFastFlags     uint64
+	PreFastDomainSet string
 }
