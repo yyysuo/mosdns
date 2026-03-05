854f183fe155bddf23b4637a16987fc0b5587ccd
yyysuo <philhorse.cn@gmail.com>
2026-03-05 11:18:17 +0800

    配置扁平化

--- plugin/matcher/qtype/qtype.go
+++ plugin/matcher/qtype/qtype.go
@@ -1,41 +1,76 @@
-/*
- * Copyright (C) 2020-2022, IrineSistiana
- *
- * This file is part of mosdns.
- *
- * mosdns is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * mosdns is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package qtype
 
 import (
+	"context"
+	"strconv"
+	"strings"
+
 	"github.com/IrineSistiana/mosdns/v5/pkg/query_context"
 	"github.com/IrineSistiana/mosdns/v5/plugin/executable/sequence"
-	"github.com/IrineSistiana/mosdns/v5/plugin/matcher/base_int"
 )
 
 const PluginType = "qtype"
 
 func init() {
-	sequence.MustRegMatchQuickSetup(PluginType, base_int.QuickSetup(matchQType))
+	sequence.MustRegMatchQuickSetup(PluginType, QuickSetup)
 }
 
-func matchQType(qCtx *query_context.Context, m base_int.IntMatcher) (bool, error) {
+type qtypeMatcher struct {
+	types []uint16
+}
+
+func (m *qtypeMatcher) Match(_ context.Context, qCtx *query_context.Context) (bool, error) {
+	// 保持原版逻辑：遍历所有 Question (虽然通常只有一个)
 	for _, question := range qCtx.Q().Question {
-		if m.Has(int(question.Qtype)) {
-			return true, nil
+		qt := question.Qtype
+		for _, t := range m.types {
+			if qt == t {
+				return true, nil
+			}
 		}
 	}
 	return false, nil
+}
+
+func (m *qtypeMatcher) GetFastCheck() func(qCtx *query_context.Context) bool {
+	// 预先拷贝，确保闭包安全且高效
+	tList := make([]uint16, len(m.types))
+	copy(tList, m.types)
+	return func(qCtx *query_context.Context) bool {
+		// DNS 请求通常只有一个 question，这里做极速提取
+		q := qCtx.Q()
+		if len(q.Question) == 0 {
+			return false
+		}
+		qt := q.Question[0].Qtype
+		for _, t := range tList {
+			if qt == t {
+				return true
+			}
+		}
+		// 如果有多个 question (极少见)，走完整遍历
+		if len(q.Question) > 1 {
+			for i := 1; i < len(q.Question); i++ {
+				qt := q.Question[i].Qtype
+				for _, t := range tList {
+					if qt == t {
+						return true
+					}
+				}
+			}
+		}
+		return false
+	}
+}
+
+func QuickSetup(_ sequence.BQ, s string) (sequence.Matcher, error) {
+	var types []uint16
+	for _, f := range strings.Fields(s) {
+		v, err := strconv.ParseUint(f, 10, 16)
+		if err != nil {
+			return nil, err
+		}
+		types = append(types, uint16(v))
+	}
+	return &qtypeMatcher{types: types}, nil
 }
