a044c4a2939a454328b1d18096b67385a246a818
msm <msm9527@users.noreply.github.com>
2026-03-06 11:45:10 +0800

    fix(udp_server): avoid unaligned atomic access on arm
    (cherry picked from commit c52c3ed72c3aea7c97acd417c03630afe4657cea)

--- plugin/server/udp_server/udp_server.go
+++ plugin/server/udp_server/udp_server.go
@@ -73,12 +73,16 @@ func (s *UdpServer) Close() error {
 }
 
 type SwitchPlugin interface{ GetValue() string }
-type DomainMapperPlugin interface{ FastMatch(qname string) ([]uint8, string, bool) }
+type DomainMapperPlugin interface {
+	FastMatch(qname string) ([]uint8, string, bool)
+}
 type IPSetPlugin interface{ Match(addr netip.Addr) bool }
 
 type fastCacheItem struct {
+	// Keep the atomic field first so 32-bit ARM gets 8-byte alignment.
+	expire int64
+
 	resp      []byte
-	expire    int64
 	updating  uint32
 	domainSet string
 }
@@ -116,7 +120,7 @@ func (fc *fastCache) GetOrUpdating(hash uint64, reqLen int, buf []byte) (int, in
 
 func (fc *fastCache) Store(qname string, qtype uint16, resp []byte, dset string) {
 	h := maphash.String(maphashSeed, qname) ^ uint64(qtype)
-	
+
 	// Scheme 1: Pre-bake clientTTL into the binary payload once.
 	bakedResp := make([]byte, len(resp))
 	copy(bakedResp, resp)
@@ -145,7 +149,7 @@ type fastHandler struct {
 
 func (h *fastHandler) Handle(ctx context.Context, q *dns.Msg, meta server.QueryMeta, pack func(*dns.Msg) (*[]byte, error)) *[]byte {
 	payload := h.next.Handle(ctx, q, meta, pack)
-	
+
 	if h.sw != nil && h.sw.GetValue() != "A" {
 		return payload
 	}
@@ -188,7 +192,7 @@ func StartServer(bp *coremain.BP, args *Args) (*UdpServer, error) {
 
 	socketOpt := server_utils.ListenerSocketOpts{
 		SO_REUSEPORT: true,
-		SO_RCVBUF:    2 * 1024 * 1024, 
+		SO_RCVBUF:    2 * 1024 * 1024,
 	}
 	lc := net.ListenConfig{Control: server_utils.ListenerControl(socketOpt)}
 	c, err := lc.ListenPacket(context.Background(), "udp", args.Listen)
@@ -216,19 +220,41 @@ func buildFastBypass(bp *coremain.BP, fc *fastCache) func(int, []byte, netip.Add
 
 	return func(reqLen int, buf []byte, remoteAddr netip.AddrPort) (int, int, uint64, string) {
 		once.Do(func() {
-			if p := bp.M().GetPlugin("switch15"); p != nil { sw15, _ = p.(SwitchPlugin) }
-			if p := bp.M().GetPlugin("switch5"); p != nil { sw5, _ = p.(SwitchPlugin) }
-			if p := bp.M().GetPlugin("switch6"); p != nil { sw6, _ = p.(SwitchPlugin) }
-			if p := bp.M().GetPlugin("switch1"); p != nil { sw1, _ = p.(SwitchPlugin) }
-			if p := bp.M().GetPlugin("switch7"); p != nil { sw7, _ = p.(SwitchPlugin) }
-			if p := bp.M().GetPlugin("switch2"); p != nil { sw2, _ = p.(SwitchPlugin) }
-			if p := bp.M().GetPlugin("switch12"); p != nil { sw12, _ = p.(SwitchPlugin) }
-			if p := bp.M().GetPlugin("unified_matcher1"); p != nil { dm, _ = p.(DomainMapperPlugin) }
-			if p := bp.M().GetPlugin("client_ip"); p != nil { ipSet, _ = p.(IPSetPlugin) }
+			if p := bp.M().GetPlugin("switch15"); p != nil {
+				sw15, _ = p.(SwitchPlugin)
+			}
+			if p := bp.M().GetPlugin("switch5"); p != nil {
+				sw5, _ = p.(SwitchPlugin)
+			}
+			if p := bp.M().GetPlugin("switch6"); p != nil {
+				sw6, _ = p.(SwitchPlugin)
+			}
+			if p := bp.M().GetPlugin("switch1"); p != nil {
+				sw1, _ = p.(SwitchPlugin)
+			}
+			if p := bp.M().GetPlugin("switch7"); p != nil {
+				sw7, _ = p.(SwitchPlugin)
+			}
+			if p := bp.M().GetPlugin("switch2"); p != nil {
+				sw2, _ = p.(SwitchPlugin)
+			}
+			if p := bp.M().GetPlugin("switch12"); p != nil {
+				sw12, _ = p.(SwitchPlugin)
+			}
+			if p := bp.M().GetPlugin("unified_matcher1"); p != nil {
+				dm, _ = p.(DomainMapperPlugin)
+			}
+			if p := bp.M().GetPlugin("client_ip"); p != nil {
+				ipSet, _ = p.(IPSetPlugin)
+			}
 		})
 
-		if sw15 == nil || sw15.GetValue() != "A" { return server.FastActionContinue, 0, 0, "" }
-		if reqLen < 12 { return server.FastActionContinue, 0, 0, "" }
+		if sw15 == nil || sw15.GetValue() != "A" {
+			return server.FastActionContinue, 0, 0, ""
+		}
+		if reqLen < 12 {
+			return server.FastActionContinue, 0, 0, ""
+		}
 
 		offset := 12
 		var nameBuf [256]byte
@@ -237,28 +263,41 @@ func buildFastBypass(bp *coremain.BP, fc *fastCache) func(int, []byte, netip.Add
 			l := int(buf[offset])
 			if l == 0 {
 				offset++
-				if nameLen == 0 { nameBuf[0] = '.'; nameLen = 1 }
+				if nameLen == 0 {
+					nameBuf[0] = '.'
+					nameLen = 1
+				}
 				break
 			}
-			if l&0xC0 == 0xC0 { return server.FastActionContinue, 0, 0, "" }
+			if l&0xC0 == 0xC0 {
+				return server.FastActionContinue, 0, 0, ""
+			}
 			offset++
-			if offset+l > reqLen || nameLen+l+1 > 256 { return server.FastActionContinue, 0, 0, "" }
+			if offset+l > reqLen || nameLen+l+1 > 256 {
+				return server.FastActionContinue, 0, 0, ""
+			}
 			copy(nameBuf[nameLen:], buf[offset:offset+l])
 			nameLen += l
 			nameBuf[nameLen] = '.'
 			nameLen++
 			offset += l
 		}
-		if offset+4 > reqLen { return server.FastActionContinue, 0, 0, "" }
+		if offset+4 > reqLen {
+			return server.FastActionContinue, 0, 0, ""
+		}
 
 		qtype := binary.BigEndian.Uint16(buf[offset : offset+2])
 		qname := unsafe.String(&nameBuf[0], nameLen)
 
 		if qtype == 6 || qtype == 12 || qtype == 65 {
-			if sw5 != nil && sw5.GetValue() == "A" { return server.FastActionReply, makeReject(reqLen, buf, offset+4, 0), 0, "" }
+			if sw5 != nil && sw5.GetValue() == "A" {
+				return server.FastActionReply, makeReject(reqLen, buf, offset+4, 0), 0, ""
+			}
 		}
 		if qtype == 28 {
-			if sw6 != nil && sw6.GetValue() == "A" { return server.FastActionReply, makeReject(reqLen, buf, offset+4, 0), 0, "" }
+			if sw6 != nil && sw6.GetValue() == "A" {
+				return server.FastActionReply, makeReject(reqLen, buf, offset+4, 0), 0, ""
+			}
 		}
 
 		var marks uint64
@@ -266,7 +305,9 @@ func buildFastBypass(bp *coremain.BP, fc *fastCache) func(int, []byte, netip.Add
 		if dm != nil {
 			if mList, dsName, match := dm.FastMatch(qname); match {
 				for _, v := range mList {
-					if v < 64 { marks |= (1 << v) }
+					if v < 64 {
+						marks |= (1 << v)
+					}
 				}
 				dset = dsName
 			}
@@ -274,37 +315,58 @@ func buildFastBypass(bp *coremain.BP, fc *fastCache) func(int, []byte, netip.Add
 
 		if sw1 != nil {
 			sw1Val := sw1.GetValue()
-			if (marks&(1<<1)) != 0 && sw1Val == "A" { return server.FastActionReply, makeReject(reqLen, buf, offset+4, 3), 0, "" }
-			if (marks&(1<<2)) != 0 && qtype == 1 && sw1Val == "A" { return server.FastActionReply, makeReject(reqLen, buf, offset+4, 0), 0, "" }
-			if (marks&(1<<3)) != 0 && qtype == 28 && sw1Val == "A" { return server.FastActionReply, makeReject(reqLen, buf, offset+4, 0), 0, "" }
+			if (marks&(1<<1)) != 0 && sw1Val == "A" {
+				return server.FastActionReply, makeReject(reqLen, buf, offset+4, 3), 0, ""
+			}
+			if (marks&(1<<2)) != 0 && qtype == 1 && sw1Val == "A" {
+				return server.FastActionReply, makeReject(reqLen, buf, offset+4, 0), 0, ""
+			}
+			if (marks&(1<<3)) != 0 && qtype == 28 && sw1Val == "A" {
+				return server.FastActionReply, makeReject(reqLen, buf, offset+4, 0), 0, ""
+			}
 		}
 		if sw7 != nil {
-			if (marks&(1<<5)) != 0 && sw7.GetValue() == "A" { return server.FastActionReply, makeReject(reqLen, buf, offset+4, 3), 0, "" }
+			if (marks&(1<<5)) != 0 && sw7.GetValue() == "A" {
+				return server.FastActionReply, makeReject(reqLen, buf, offset+4, 3), 0, ""
+			}
 		}
 
 		ipMatch := false
-		if ipSet != nil { ipMatch = ipSet.Match(remoteAddr.Addr().Unmap()) }
+		if ipSet != nil {
+			ipMatch = ipSet.Match(remoteAddr.Addr().Unmap())
+		}
 		sw2Val, sw12Val := "", ""
-		if sw2 != nil { sw2Val = sw2.GetValue() }
-		if sw12 != nil { sw12Val = sw12.GetValue() }
+		if sw2 != nil {
+			sw2Val = sw2.GetValue()
+		}
+		if sw12 != nil {
+			sw12Val = sw12.GetValue()
+		}
 
-		if sw2Val == "A" && sw12Val == "B" && !ipMatch { marks |= (1 << 39)
-		} else if sw2Val == "B" && sw12Val == "A" && ipMatch { marks |= (1 << 39) }
+		if sw2Val == "A" && sw12Val == "B" && !ipMatch {
+			marks |= (1 << 39)
+		} else if sw2Val == "B" && sw12Val == "A" && ipMatch {
+			marks |= (1 << 39)
+		}
 
 		if (marks & (1 << 39)) == 0 {
-			// Scheme 2 Correct Implementation: 
-			// Consistent hashing with Store by using maphash.String directly 
+			// Scheme 2 Correct Implementation:
+			// Consistent hashing with Store by using maphash.String directly
 			// on the already constructed qname string.
 			hKey := maphash.String(maphashSeed, qname) ^ uint64(qtype)
 			action, rLen, _, ds := fc.GetOrUpdating(hKey, reqLen, buf)
-			if action == server.FastActionReply { return action, rLen, 0, ds }
+			if action == server.FastActionReply {
+				return action, rLen, 0, ds
+			}
 		}
 		return server.FastActionContinue, 0, marks, dset
 	}
 }
 
 func makeReject(reqLen int, buf []byte, offset int, rcode byte) int {
-	if offset > reqLen { offset = reqLen }
+	if offset > reqLen {
+		offset = reqLen
+	}
 	buf[2] |= 0x80
 	buf[3] |= 0x80
 	buf[3] = (buf[3] & 0xF0) | (rcode & 0x0F)
@@ -312,14 +374,26 @@ func makeReject(reqLen int, buf []byte, offset int, rcode byte) int {
 }
 
 func findTTLOffsets(msg []byte) []int {
-	if len(msg) < 12 { return nil }
+	if len(msg) < 12 {
+		return nil
+	}
 	qdcount := binary.BigEndian.Uint16(msg[4:6])
 	ancount := binary.BigEndian.Uint16(msg[6:8])
-	if ancount == 0 { return nil }
+	if ancount == 0 {
+		return nil
+	}
 	offset := 12
 	for i := 0; i < int(qdcount); i++ {
 		for offset < len(msg) {
-			l := int(msg[offset]); if l == 0 { offset++; break }; if l&0xC0 == 0xC0 { offset += 2; break }
+			l := int(msg[offset])
+			if l == 0 {
+				offset++
+				break
+			}
+			if l&0xC0 == 0xC0 {
+				offset += 2
+				break
+			}
 			offset += l + 1
 		}
 		offset += 4
@@ -327,12 +401,25 @@ func findTTLOffsets(msg []byte) []int {
 	var offsets []int
 	for i := 0; i < int(ancount); i++ {
 		for offset < len(msg) {
-			l := int(msg[offset]); if l == 0 { offset++; break }; if l&0xC0 == 0xC0 { offset += 2; break }
+			l := int(msg[offset])
+			if l == 0 {
+				offset++
+				break
+			}
+			if l&0xC0 == 0xC0 {
+				offset += 2
+				break
+			}
 			offset += l + 1
 		}
-		if offset+10 > len(msg) { break }
-		offset += 4; offsets = append(offsets, offset); offset += 4
-		rdlen := binary.BigEndian.Uint16(msg[offset : offset+2]); offset += 2 + int(rdlen)
+		if offset+10 > len(msg) {
+			break
+		}
+		offset += 4
+		offsets = append(offsets, offset)
+		offset += 4
+		rdlen := binary.BigEndian.Uint16(msg[offset : offset+2])
+		offset += 2 + int(rdlen)
 	}
 	return offsets
 }
